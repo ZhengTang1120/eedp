2f525763443c2a32ec56110681270b7f0814da47
Zheng Tang <oscartang1120@gmail.com>
2018-09-22 02:39:16 -0700

    ...

--- eval.py
+++ eval.py
@@ -11,20 +11,25 @@ from multiprocessing import Pool
 
 def evaluate(i):
 	
-	os.system(f'python parse.py outmp21/parser{i:03} brat_dev.conllx bratdevout{i:03}.conllx')
-	os.system(f'python conllx2brat.py bionlp09_shared_task_development_data_rev1/ --conllx bratdevout{i:03}.conllx --outfolder {i:03}')
-	output = subprocess.run(f'./a2-evaluate.pl -g gold-dev/ -s -p {i:03}/*.t1', stdout=subprocess.PIPE, shell = True).stdout.decode("utf-8")
+	os.system(f'python parse.py out13_2/parser{i:03} brat_dev13.conllx bratdevout13.conllx')
+	os.system(f'python conllx2brat.py BioNLP-ST-2013_GE_devel_data_rev3/ --conllx bratdevout13.conllx')
+	os.system(f'./a2-evaluate.pl -g BioNLP-ST-2013_GE_devel_data_rev3 -s -p BioNLP-ST-2013_GE_devel_data_rev3_out/*.a2')
+	# output = subprocess.run(f'./a2-evaluate.pl -g BioNLP-ST-2013_GE_devel_data_rev3/ -s -p 2013_GE_devel_data_rev3_out/*.a2', stdout=subprocess.PIPE, shell = True).stdout.decode("utf-8")
 
-	with open(f'results_{i:03}.txt', 'w') as f:
-		print(output, file=f)
+	# with open(f'results_{i:03}.txt', 'w') as f:
+	# 	print(output, file=f)
+
+
 
 
 if __name__ == '__main__':
-	n_procs = int(sys.argv[1])
-	start = int(sys.argv[2])
-	end = int(sys.argv[3]) + 1
-	starttime = time.time()
-	with Pool(n_procs) as pool:
-		pool.map(evaluate, range(start, end))
-	endtime = time.time()
-	print(f'time: {endtime-starttime:,.2f} secs')
+	# n_procs = int(sys.argv[1])
+	# start = int(sys.argv[2])
+	# end = int(sys.argv[3]) + 1
+	# starttime = time.time()
+	# with Pool(n_procs) as pool:
+	# 	pool.map(evaluate, range(start, end))
+	# endtime = time.time()
+	# print(f'time: {endtime-starttime:,.2f} secs')
+	for i in range(1,50):
+		evaluate(i)
